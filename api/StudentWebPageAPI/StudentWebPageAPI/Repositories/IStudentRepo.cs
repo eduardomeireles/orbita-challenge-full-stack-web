171cda07e7393a713900975d8e10ba5962b98eba
eduardomeireles <eduardode.meireles@gmail.com>
2022-10-24 19:41:41 -0300

    feat/Student Interface

---
+++ api/StudentWebPageAPI/StudentWebPageAPI/Repositories/IStudentRepo.cs
@@ -0,0 +1,12 @@
+﻿using StudentWebPageAPI.Models;
+
+namespace StudentWebPageAPI.Repositories
+{
+    public interface IStudentRepo
+    {
+        Task<List<Student>> GetAll();
+        Task<bool> Create(Student newStudent);
+        Task<bool> Update(Student selected);
+        Task<bool> Delete(Student selected);
+    }
+}
